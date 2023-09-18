d0f9a09b3e5e2650f8ce0535cff18712ff055a00
Ediblelnk <peters1731@gmail.com>
2023-09-18 01:12:37 -0400

    lint

--- csc328/project2/project2.c
+++ csc328/project2/project2.c
@@ -1,34 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-//#include <sys/wait.h>
+// #include <sys/wait.h>
 
-int X; //global variable
+int X; // global variable
 
-void fatal_error(char code, int num) {
-  switch(code) {
-    case 'i': //INPUT ERROR
-      printf("INPUT ERROR: EXPECTED 4 OR 5 INPUTS, GOT %d.\n", num-1);
-      break;
-  }
+void fatal_error(char code, int num)
+{
+    switch (code)
+    {
+    case 'i': // INPUT ERROR
+        printf("INPUT ERROR: EXPECTED 4 OR 5 INPUTS, GOT %d.\n", num - 1);
+        break;
+    }
 
-  exit(1);
+    exit(1);
 }
 
-int main(int argc, char** argv) {
-  //ensure proper number of arguments
-  if(argc < 5 || 6 < argc) fatal_error('i', argc);
-  
-  //assign output to either file or to console
-  FILE* fp = (argc == 6) ? fopen(argv[5], "w") : stdout;
+int main(int argc, char **argv)
+{
+    // ensure proper number of arguments
+    if (argc < 5 || 6 < argc)
+        fatal_error('i', argc);
 
-  //assign variables from input
-  int num_forks = atoi(argv[1]);
-  X = atoi(argv[2]);
-  int num = atoi(argv[3]);
-  int nump = atoi(argv[4]);
+    // assign output to either file or to console
+    FILE *fp = (argc == 6) ? fopen(argv[5], "w") : stdout;
 
-  printf("%d, %d, %d, %d", num_forks, X, num, nump);
+    // assign variables from input
+    int num_forks = atoi(argv[1]);
+    X = atoi(argv[2]);
+    int num = atoi(argv[3]);
+    int nump = atoi(argv[4]);
 
-  return 0;
+    printf("%d, %d, %d, %d", num_forks, X, num, nump);
+
+    return 0;
 }
