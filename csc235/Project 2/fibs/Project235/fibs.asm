59a83860e520211793de994dc0ed2e918ad75117
Ediblelnk <peters1731@gmail.com>
2023-10-02 18:18:35 -0400

    restruct

---
+++ csc235/Project 2/fibs/Project235/fibs.asm
@@ -0,0 +1,16 @@
+; 
+; Find first 25 Fibonacci numbers
+; print them in reverse order
+; 
+; Name:
+
+include Irvine32.inc
+
+.data
+
+.code
+main PROC
+
+	invoke ExitProcess,0			; 'bye
+main ENDP
+END main
