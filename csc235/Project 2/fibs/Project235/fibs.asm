c00ed307f26479fa862e1c9c7adce8c52bfc3899
Ediblelnk <peters1731@gmail.com>
2023-10-02 23:22:47 -0400

    (basically) finished project 2

--- csc235/Project 2/fibs/Project235/fibs.asm
+++ csc235/Project 2/fibs/Project235/fibs.asm
@@ -2,15 +2,38 @@
 ; Find first 25 Fibonacci numbers
 ; print them in reverse order
 ; 
-; Name:
+; Name: Peter Schaefer
 
 include Irvine32.inc
 
 .data
+NUM = 25
+
+fibs DWORD 2 DUP(1), NUM-2 DUP(?)	; NUM number of dwords
 
 .code
 main PROC
+	mov ecx, NUM - 2						; prepare the loop, two less
+	mov esi, OFFSET fibs + 2 * TYPE fibs	; already set 2 fib nums
 
-	invoke ExitProcess,0			; 'bye
+FIB_FILL:
+	mov eax, [esi - 2 * TYPE fibs]	; value of second prior number
+	add eax, [esi - TYPE fibs]		; value of first prior number
+	mov [esi], eax					; set value of next fib number
+	add esi, TYPE fibs				; increment to next place
+	loop FIB_FILL
+
+	sub esi, TYPE fibs	; move back to last fib number
+	mov ecx, NUM		; reset loop counter
+
+FIB_PRINT:
+	mov eax, [esi]		; load value of last fib number
+	call WriteDec		; write to output
+	call Crlf			; newline
+	sub esi, TYPE fibs	; go to previous fib number
+	loop FIB_PRINT
+
+	call WaitMsg
+	invoke ExitProcess,0		; bye
 main ENDP
 END main
