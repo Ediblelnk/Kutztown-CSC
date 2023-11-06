b533df9dad5fb5f0314158fc2f31788fc2b0be34
Ediblelnk <peters1731@gmail.com>
2023-11-06 09:28:34 -0500

    project 6, yea bby!

--- csc328/project6/src/main.rs
+++ csc328/project6/src/main.rs
@@ -1,3 +1,10 @@
+/**
+ * Filename:	main.rs
+ * Author:	    Schaefer, Peter
+ * Assignment: 	Fall 2023, Project 6: Sockets (Server)
+ * Course: 	    CSC 328 Network and Secure Programming
+ * Professor:	Schwesinger
+ */
 use rand::random;
 use std::{
     env,
@@ -9,15 +16,19 @@ use std::{
 
 const ERR: i32 = -1;
 
+/**
+ * Description: main program. takes exactly 1 command line argument.
+ *              demonstrates binding, listening, and finally connecting to a socket.
+ *              uses the provided port and hosts a server on localhost:port.
+ * Argument:    <port>  :   a port number between 10000 and 65535.
+ */
 fn main() {
-    let words: Vec<&str> = "the quick brown fox jumps over the lazy dog is an english language pangram a sentence that contains all the letter of the alphabet the phrase is commonly used for touch typing practice testing typewriters and computer keyboards displaying examples of fonts and other applications involving text where the use of all letter in the alphabet is used"
+    let word_bank: Vec<&str> = "aloof besought cunning-handed dingle eyot flummoxed gloaming hearken inaureoled jetsam kindle lissom mattock noisome obeisance parapet quaff raiment sunder thraldom unsullied vassal wheedle yammer zenith"
         .split(" ")
         .collect();
 
-    ctrlc::set_handler(move || println!("Recieved Ctrl+C!")).expect("Error setting Ctrl+C handler");
-
+    // set up and confirm the command arguments
     let args: Vec<String> = env::args().collect();
-
     if args.len() != 2 {
         eprintln!("USAGE: ./server <port>\n\t[where 10000 < port < 65535]");
         process::exit(ERR);
@@ -28,6 +39,7 @@ fn main() {
         process::exit(ERR);
     });
 
+    //set up TcpListener, bind to port, and listen for connections
     let listener: TcpListener =
         TcpListener::bind(format!("localhost:{port}")).unwrap_or_else(|why| {
             eprintln!("ERROR: {why}");
@@ -35,16 +47,15 @@ fn main() {
         });
 
     println!(
-        "INFO: Server started on address {:?}",
+        "INFO: Server started on and listening for connections on address {:?}",
         listener.local_addr().unwrap()
     );
-    println!("INFO: Listening for connections...");
 
     for stream in listener.incoming() {
         match stream {
             Ok(mut s) => {
                 println!("INFO: Got a connection from {:?}", s.peer_addr().unwrap());
-                send_words(&mut s, &words);
+                send_words(&mut s, &word_bank);
             }
             Err(why) => {
                 eprintln!("ERROR: {why}");
@@ -54,8 +65,14 @@ fn main() {
     }
 }
 
+/**
+ * Description: sends 1 to 10 random word packets to a client.
+ *              each word packet has two parts:
+ *                  the length of the string, a u16 in big endian (network) byte order
+ *                  the ASCII characters, a sequence of bytes
+ */
 fn send_words(s: &mut TcpStream, word_bank: &Vec<&str>) {
-    for _ in 0..(1 + random::<u8>() % 10) {
+    for _ in 0..(1 + random::<usize>() % 10) {
         let word = word_bank[random::<usize>() % word_bank.len()];
         s.write_all(&(word.len() as u16).to_be_bytes()).unwrap();
         s.write_all(word.as_bytes()).unwrap();
